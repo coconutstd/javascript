4d2c18ff110d07129172748c23513bad2c208140
junui <wnsspdl@naver.com>
2021-09-10 08:58:27 +0900

    비동기인 코드를 순차적으로 엮으려면 콜백을 사용해야함

--- reference/동기코드와비동기코드.js
+++ reference/동기코드와비동기코드.js
@@ -1,4 +1,17 @@
+function double(x) {
+    return x * 2;
+}
 
+function calcVal(a, b, cb) {
+    setTimeout(() => {
+        cb(a + b);
+    }, 100)
+}
 
 let a = double(100);
 let b = a;
+
+const r = calcVal(1, 2, (result) => {
+    console.log(result);
+});
+const q = r;
