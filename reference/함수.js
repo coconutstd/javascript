56483c56672dd41d4490f40a45afc8104f95d466
junui <wnsspdl@naver.com>
2021-09-17 17:45:08 +0900

    자바스크립트에서 함수는 값으로도 취급된다.

---
+++ reference/함수.js
@@ -0,0 +1,12 @@
+// 아래와 달리 함수 정의문이다.
+function myFunction(x) {
+    return x + 100;
+}
+
+
+// 자바스크립트에서 함수는 식(=값)으로 취급된다.
+const myFunction2 = function () {
+    return x + 100;
+};
+
+myFunction2();
