f7af79154a11facd9563a8a3e3fe11f08974f879
junui <wnsspdl@naver.com>
2021-09-26 12:00:14 +0900

    일급함수는 값으로써 함수를 취급한다는 의미이다

---
+++ reference/일급함수/인자로넘겨주는함수.ts
@@ -0,0 +1,26 @@
+function ul(child: string): string {
+    return `<ul>${child}</ul>`;
+}
+
+function ol(child: string): string {
+    return `<ol>${child}</ol>`;
+}
+
+function makeLi(
+    container: (child: string) => string,
+    contents: string[]): string {
+    
+    const liList = [];
+
+    for(const content of contents){
+        liList.push(`<li>${content}</li>`);
+    }
+    
+    return container(liList.join(''));
+}
+
+const htmlUl = makeLi(ul, ['월', '화', '수']);
+const htmlOl = makeLi(ol, ['1', '2', '3']);
+
+console.log(htmlUl);
+console.log(htmlOl);
