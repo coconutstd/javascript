d8e6494981e2f6e291a728a860acc33945fbd3cb
junui <wnsspdl@naver.com>
2021-09-29 14:34:06 +0900

    SIGNIN 서버 완성

--- signin/server/index.js
+++ signin/server/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const axios = require('axios');
+const { V4MAPPED } = require('dns');
 const app = express();
 
 app.get('/api/user/:id', (req, res) => {
@@ -10,5 +11,62 @@ app.get('/api/user/:id', (req, res) => {
         });
     }
 
-    
+    axios.get('https://randomuser.me/api')
+        .then(result => {
+            const [userProfile] = result.data.results;
+            const { name, picture, phone, email, country } = userProfile;
+
+            res.status(200).send({
+                status: 'OK',
+                result: {
+                    name, picture, phone, email, country
+                }
+            })
+        })
+        .catch(e => {
+            res.status(400).send({
+                status: 'Error'
+            });
+        });
 });
+
+app.get('/api/user/:id/posts', (req, res) => {
+    if (!req.headers['token']) {
+        return res.status(403).send({
+            status: 'Error'
+        });
+    }
+
+    axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${req.params.id}`)
+        .then(result => {
+            res.status(200).send({
+                status: 'OK',
+                results: result.data
+            });
+        })
+        .catch(e => {
+            res.status(400).send({
+                status: 'Error'
+            });
+        });
+});
+
+app.post('/api/authentication', (req, res) => {
+    if (Math.floor(Math.random() * 10) % 2 === 0) {
+        res.status(200).send({
+            status: 'OK',
+            result: {
+                id: Math.floor(Math.random()*10),
+                token: v4(),
+            }
+        });
+    } else {
+        res.status(400).send({
+            status: 'Error'
+        });
+    }
+});
+
+app.listen(8080, () => {
+    console.log('ready to dummy signup server');
+})
