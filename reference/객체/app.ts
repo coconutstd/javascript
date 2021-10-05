6bf5bf9f53dbf36eec76e9f722c0dccc8fda810d
junui <wnsspdl@naver.com>
2021-10-05 18:00:08 +0900

    함수로 객체를 생성하면 데이터와 스킴을 분리할 수 있다

--- reference/객체/app.ts
+++ reference/객체/app.ts
@@ -5,6 +5,7 @@ type Box = {
     backgroundColor: string;
     borderWidth?: number;
     ['className']?: string;
+    color?: string;
 };
 
 let box: Box = {
@@ -13,3 +14,45 @@ let box: Box = {
     borderRadius: 5,
     backgroundColor: 'red'
 };
+
+function makeBox(
+    width: number,
+    height: number,
+    borderRadius: number,
+    backgroundColor: string,
+): Box {
+    return {
+        width,
+        height,
+        borderRadius,
+        backgroundColor
+    }
+}
+
+const box1 = makeBox(100, 200, 10, 'blue');
+
+class Shape implements Box {
+    width: number;
+    height: number;
+    borderRadius: number;
+    backgroundColor: string;
+    color: string;
+
+    constructor(
+        width: number,
+        height: number,
+        borderRadius: number,
+        backgroundColor: string
+    ){
+        this.width = width;
+        this.height = height;
+        this.borderRadius = borderRadius;
+        this.backgroundColor = backgroundColor;
+    }
+}
+
+const boxShape = new Shape(100,200,30,'red');
+boxShape.color = 'black';
+
+const box2 = Object.assign({}, box);
+const box3 = {...box2, color : 'yellow'};
